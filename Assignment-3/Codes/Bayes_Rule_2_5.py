0f0c8ed9851138613c725c2db8e258d85c6bc66c
Tuhin Dutta <dt_tauhait@hotmail.com>
2021-01-20 05:42:13 +0530

    Submit Assignment-3

---
+++ Assignment-3/Codes/Bayes_Rule_2_5.py
@@ -0,0 +1,49 @@
+def bayes_theorem(P_E, P_F_given_E, P_F):
+    return (P_E * P_F_given_E) / P_F
+
+def calc_P_F_given_E(P_E_and_F, P_E):
+    return P_E_and_F / P_E
+
+def calc_P_E_given_F(P_E_and_F, P_F):
+    return P_E_and_F / P_F
+    
+def bothChildMale_given_at_least_one_male():    
+    # Prior or P(E) = both children to be males
+    P_E = 0.25
+    
+    # Evidence or P(F) = at least one children to be male
+    P_F = 0.75
+    
+    # P(E and F)
+    P_E_and_F = 0.25
+    
+    # Likelihood or P( F|E )
+    P_F_given_E = calc_P_F_given_E(P_E_and_F, P_E)
+    
+    # Posterior or P( E|F )
+    P_E_given_F = bayes_theorem(P_E, P_F_given_E, P_F)
+    
+    print('\n The probability of both children to be males ' +
+          'given that at least one child to be male = %.2f \n' % P_E_given_F)
+
+def bothChildFemale_given_elder_female():    
+    # Prior or P(E) = both children to be females
+    P_E = 0.25
+    
+    # Evidence or P(F) = elder child being a female
+    P_F = 0.5
+    
+    # P(E and F)
+    P_E_and_F = 0.25
+    
+    # Likelihood or P( F|E )
+    P_F_given_E = calc_P_F_given_E(P_E_and_F, P_E)
+    
+    # Posterior or P( E|F )
+    P_E_given_F = bayes_theorem(P_E, P_F_given_E, P_F)
+    
+    print('\n The probability that both children are females, ' + 
+          'given that the elder child is a female = %.2f\n' % P_E_given_F)
+
+bothChildMale_given_at_least_one_male()
+bothChildFemale_given_elder_female()
