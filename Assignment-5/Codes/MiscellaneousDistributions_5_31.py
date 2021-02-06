65180c5f531a1edfcb87789b8f5b87b99bfec3b8
Tuhin Dutta <dt_tauhait@hotmail.com>
2021-02-06 12:13:27 +0530

    Code and plot modifications

--- Assignment-5/Codes/MiscellaneousDistributions_5_31.py
+++ Assignment-5/Codes/MiscellaneousDistributions_5_31.py
@@ -9,33 +9,41 @@ import numpy as np
 from scipy.stats import bernoulli
     
 def sim():
+    np.random.seed(0)
     n_rvs = 3
     mean = 34/221
     std = np.sqrt(6800/48841)
-    p_x = np.array([188/221, 32/221, 1/221])
-    sim_len = int(1e6)
-    sim_bern_arr = np.zeros(shape=(n_rvs, sim_len), dtype=float)
-    sim_counter = np.zeros(shape=n_rvs, dtype=float)
+    p_x = np.array([np.float64(188/221), np.float64(32/221), np.float64(1/221)])
+    sim_len = int(1e9)
+    sim_counter = np.zeros(shape=n_rvs, dtype=int)
     sim_pmf = np.zeros(shape=n_rvs, dtype=float)
     sim_cdf = np.zeros(shape=n_rvs, dtype=float)
+    remain_samples = sim_len
     for i in range(n_rvs):
-        sim_bern_arr[i, :] = bernoulli.rvs(size=sim_len, p=p_x[i])
+        sim_bern_arr = bernoulli.rvs(size=remain_samples, p=p_x[i])    
+        sim_counter[i] = np.count_nonzero(sim_bern_arr==1)        
+        sim_pmf[i] = np.float64(sim_counter[i]/remain_samples)
+        remain_samples = remain_samples - sim_counter[i]
     
-    sim_counter = np.sum(sim_bern_arr, axis=1)
-    sim_pmf = sim_counter / sim_len
     sim_cdf = np.cumsum(sim_pmf)
     x = np.array([0, 1, 2])
     y1 = np.array([0.851, 0.145, 0.004])
     y2 = np.array([0.851, 0.996, 1.0])
-    plot(x, y1, sim_pmf, mean, std, 'blue', 'red', 'Theory Vs Simulation PMf')
+    print('The simulation PMf values are: ', np.round(sim_pmf, 3))
+    print('The CDF values are: ', np.round(sim_cdf, 3))
+    plot(x, y1, sim_pmf, mean, std, 'blue', 'red', 'Theory Vs Simulation PMf', False)
     print('\n\n')
-    plot(x, y2, sim_cdf, mean, std, 'yellow', 'black', 'Theory Vs Simulation CDF')
+    plot(x, y2, sim_cdf, mean, std, 'yellow', 'black', 'Theory Vs Simulation CDF', True)
 
-def plot(x, dist1, dist2, mean, std, col1, col2, t):    
-    plt.stem(x, dist2, linefmt='C1-', markerfmt='C1o', label='Simulation')
-    plt.stem(x, dist1, linefmt='C0-', markerfmt='C0.', label='Theory')
+def plot(x, dist1, dist2, mean, std, col1, col2, t, flag_):    
+    if flag_ is True:
+        plt.plot(x, dist2, color='green', marker='o', markersize=12, label='Simulation')
+        plt.plot(x, dist1, color='blue', marker='o', markersize=6, label='Theory')
+    else:
+        plt.stem(x, dist2, linefmt='C1-', markerfmt='C1o', label='Simulation')
+        plt.stem(x, dist1, linefmt='C0-', markerfmt='C0.', label='Theory')
+    
     plt.grid(color='y', linestyle='--', linewidth=2)
-    plt.ylim(0, 1.25)
     plt.legend()
     plt.title(t)
     plt.show()
