1e30afd0256a72cc938c057d603aebab7c826335
Tuhin Dutta <dt_tauhait@hotmail.com>
2021-02-07 22:37:42 +0530

    Modify code

--- Assignment-5/Codes/MiscellaneousDistributions_5_31.py
+++ Assignment-5/Codes/MiscellaneousDistributions_5_31.py
@@ -6,43 +6,44 @@ Created on Sat Jan 30 18:49:47 2021
 """
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import bernoulli
+import math
+from scipy.stats import binom
     
-def sim():
-    n_rvs = 3
-    mean = 34/221
-    std = np.sqrt(6800/48841)
+def sim(precision, num_rvs_gen):
+    n_rv = 3
     p_x1 = np.float64(188/221)
     p_x2 = np.float64(32/221)
     p_x3 = np.float64(1/221)
     
-    sim_len = int(1e9)
-    sim_bern_arr = np.zeros(shape=(n_rvs, sim_len), dtype=int)
-    sim_bern_arr[0] = bernoulli.rvs(size=sim_len, p=p_x1)
-    sim_bern_arr[1] = bernoulli.rvs(size=sim_len, p=p_x2)
-    sim_bern_arr[2] = bernoulli.rvs(size=sim_len, p=p_x3)
+    prec = precision
+    rvs_size = num_rvs_gen
+    sim_len = int(math.pow(10, prec))
     
-    sim_counter = np.zeros(shape=n_rvs, dtype=int)
-    sim_pmf = np.zeros(shape=n_rvs, dtype=np.float64)
-    sim_cdf = np.zeros(shape=n_rvs, dtype=np.float64)
-    for i in range(n_rvs):
-        sim_counter[i] = np.count_nonzero(sim_bern_arr[i]==1)
-        temp = sim_counter[i]/sim_len
-        sim_pmf[i] = temp
+    sim_bern_arr = np.zeros(shape=(n_rv, rvs_size), dtype=int)
+    
+    sim_bern_arr[0] = binom.rvs(n=sim_len, p=np.round(p_x1, prec), size=rvs_size)
+    sim_bern_arr[1] = binom.rvs(n=sim_len, p=np.round(p_x2, prec), size=rvs_size)
+    sim_bern_arr[2] = binom.rvs(n=sim_len, p=np.round(p_x3, prec), size=rvs_size)
+    
+    sim_pmf = np.zeros(shape=n_rv, dtype=np.float64)
+    sim_cdf = np.zeros(shape=n_rv, dtype=np.float64)
+    for i in range(n_rv):        
+        sim_pmf[i] = np.round(sim_bern_arr[i].mean()/sim_len, prec)
     
     sim_cdf = np.cumsum(sim_pmf)
     x = np.array([0, 1, 2])
-    y1 = np.array([p_x1, p_x2, p_x3])
-    y2 = np.array([p_x1, p_x1 + p_x2, p_x1 + p_x2 + p_x3])
+    y1 = np.array([np.round(p_x1, prec), np.round(p_x2, prec), np.round(p_x3, prec)])
+    y2 = np.array([y1[0], y1[0]+y1[1], y1[0]+y1[1]+y1[2]])
+    print('Precision is set to ', prec, ' digits.\n')
     print('The theoretical PMf values are: ', y1)
     print('The simulation PMf values are: ', sim_pmf)
-    print('The theoretical CDF values are: ', y2)
+    print('\nThe theoretical CDF values are: ', y2)
     print('The simulated CDF values  are: ', sim_cdf)
-    plot(x, y1, sim_pmf, mean, std, 'blue', 'red', 'Theory Vs Simulation PMf', False)
+    plot(x, y1, sim_pmf, 'blue', 'red', 'Theory Vs Simulation PMf', False)
     print('\n\n')
-    plot(x, y2, sim_cdf, mean, std, 'yellow', 'black', 'Theory Vs Simulation CDF', True)
+    plot(x, y2, sim_cdf, 'yellow', 'black', 'Theory Vs Simulation CDF', True)
 
-def plot(x, dist1, dist2, mean, std, col1, col2, t, flag_):    
+def plot(x, dist1, dist2, col1, col2, t, flag_):    
     if flag_ is True:
         plt.plot(x, dist2, color='green', marker='o', markersize=12, label='Simulation')
         plt.plot(x, dist1, color='blue', marker='o', markersize=6, label='Theory')
@@ -55,4 +56,4 @@ def plot(x, dist1, dist2, mean, std, col1, col2, t, flag_):
     plt.title(t)
     plt.show()
 
-sim()
+sim(4, int(1e5))
